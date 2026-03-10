a0b6bc9a68e75774de5089ba4e780e27d01123e9
Zoltan <zoltan.frenyo2@gmail.com>
2026-03-10 16:19:39 +0100

    finished iteration.py and socket_connection.py

--- functions/iteration.py
+++ functions/iteration.py
@@ -1,10 +1,57 @@
 from typing import List, Tuple
+from socket_connection import socket_connection
+import logging
 
-def port_scanner(scan_input: tuple[str,List[int]]) -> tuple[int,bool]:
-    #loop through the list of ports and for each one call port_scan(ip_address,port)
-    #save the result for each port (port: int, is_open: bool)
-    #return the result
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def iteration(scan_input: tuple[str,List[int]]) -> List[tuple[int,bool]]:
+    """
+        Loops through a list of ports and attempts to connect to each of them on the
+        specified IP address
+
+        Inputs:
+            A tuple composed of:
+                An IP address (str)
+                A list of ports to check (List[int])
+
+        Output:
+            A list of tuples composed of:
+                A port (int)
+                Whether the port is open or not (bool)
+    """
+    
+    #Gather the ip address
+    ip_address: str = scan_input[0]
+
+    #Gather the list of ports to scan
+    input_port_list: List[int] = scan_input[1]
+
+    #Create the scan result output list (empty at first)
+    output_port_list: List[tuple[int,bool]] = []
+
+    logging.info(f"starting port scan on address {ip_address} and range {input_port_list[0]} - {input_port_list[-1]}")
+
+    #Loop through the list of ports
+    for port in input_port_list:
+        # Try to connect to the port on the specified ip address
+        # Gather the result (true/false)
+        is_open: bool = socket_connection(ip_address, port)
+
+        #Store the scanned port and the result of the scan in a single variable
+        port_status: tuple[int,bool] = (port,is_open)
+
+        #Add the result to the output list
+        output_port_list.append(port_status)
+
+    logging.info(f"port scan finished!")
+
+    return output_port_list
 
 
-    #added line
-    return     
+test = ("192.168.56.101",[0,22,23,60])
+result = iteration(test)
+print(result)
--- functions/socket_connection.py
+++ functions/socket_connection.py
@@ -1,8 +1,64 @@
 from typing import List, Tuple
+import socket
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 def socket_connection(ip_address: str, port: int) -> bool:
-    #Tries to connect to the remote port on the target IP
-    #Raises an exception if there is an error
-    #Writes the result in a bool (true/false)
-    #Returns the result
-    return
+    """
+        Attempts to connect to a single TCP port
+
+        Inputs:
+            A target IP address (str)
+            A target TCP port number (int)
+
+        Output:
+            A boolean indicating if the port is open
+    """
+    # Creates a socket (necessary for a network connection)
+    # The arguments specify the format (outside of the scope of this project)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Sets a timeout after which the accempt is interrupted
+    sock.settimeout(1)
+
+    # Do a "try" to handle exceptions raised during the connection attempt
+    try:
+            # Try to connect to a specified port on a specified ip address
+            # Returns an integer, 0 = success, otherwise => error code
+            logging.info(f"scanning {ip_address} : {port} ...")
+
+            result = sock.connect_ex((ip_address, port))
+
+            # We check the result, if it's 0 the port is open, otherwise it is closed
+            is_open: bool = (result == 0)
+
+            #print("attempt succeeded")
+            if is_open: logging.info("finished - port open") 
+            else: logging.info("finished - port closed")
+            
+
+            return is_open
+    
+    # Exception - Name resoltuion failed
+    except socket.gaierror:
+            logging.debug("aborted - name resolution failed")
+            return False
+    
+    # Exception - Network/socket error
+    except OSError:
+            logging.debug("aborted - network/socket error")
+            return False
+    finally:
+            sock.close()
+
+
+#is_open = socket_connection("192.168.56.101",22)
+#print(is_open)
+
+
+
