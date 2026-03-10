a1388f36e82c6fc392998c16ce047083b0d36b1d
Zoltan <zoltan.frenyo2@gmail.com>
2026-03-10 13:52:26 +0100

    created take_input()

--- functions/take_input.py
+++ functions/take_input.py
@@ -1,9 +1,11 @@
 from typing import List, Tuple
 
-def take_input() -> tuple[str,List[int]]:
+def take_input() -> tuple[str,str]:
     #Ask the user to enter an ip address
     #Store it in a variable (Str)
+    ip_address = input("Enter the IP address: ")
     #Ask the user to enter a list of ports
-    #Store it in a variable (List of int)
+    #Store it in a variable (Str)
+    ports = input("Enter the port range: ")
     #Return the result
-    return
+    return ip_address, ports
